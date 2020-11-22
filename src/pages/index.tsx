45e1f625b3db7a8e21e676dcae1f024385215d20
caioaugustoo <caioamfr@gmail.com>
2020-11-22 17:24:48 -0300

    Listagem dos componentes no index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
 import Header from "../components/Header";
+import HomeSection from "../components/Home";
+import About from "../components/About";
+import ScrollTop from "../components/ScrollTop";
+import ScrollDown from "../components/ScrollDown";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <HomeSection />
+      <ScrollDown />
+      <About />
+      <ScrollTop />
+    </>
+  );
 }
