1f771a54e0717f39e1d4ab4ee9d3a3a9713b2a79
caioaugustoo <caioamfr@gmail.com>
2020-11-22 01:55:03 -0300

    Alerada importações e alterada cor

---
+++ src/style/theme/global.ts
@@ -0,0 +1,36 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import { theme } from './themes'
+
+// Resets
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box
+  }
+  :root {
+    font-size: 62.5%;
+  }
+  body {
+    font-family: 'Montserrat', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  ul li {
+    list-style: none;
+  }
+
+  a { 
+    text-decoration: none;
+    color: ${theme.colors.black}
+  }
+`;
+export default GlobalStyles;
+
+// Styles
+export const Container = styled.div`
+  max-width: 120rem;
+  padding: 0 4rem;
+  margin: 0 auto;
+  width: 100%;
+`;
---
+++ src/style/theme/themes.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  colors: {
+    black: '#111827',
+    blue: '#00aaff',
+    white: '#ffffff'
+  }
+}
