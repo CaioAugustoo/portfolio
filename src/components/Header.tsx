557956fc2e7d6c70ec0848527632a25a4ddd53d1
caioaugustoo <caioamfr@gmail.com>
2020-11-22 03:49:29 -0300

    Classe adicionada quando menu aberto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import Image from "next/image";
+import * as S from "./styles";
+
+import { Container } from "../style/theme/global";
+import { useState } from "react";
 
 const Header = () => {
-  return <div></div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <S.Header>
+      <Container>
+        <S.ItensWrapper>
+          <Image
+            src="/img/logo.svg"
+            alt="Logo com as inicias 'C A'"
+            width={35}
+            height={37}
+            quality={100}
+            loading="eager"
+          />
+
+          <nav>
+            <S.NavItens className={isMenuOpen ? "menu_active" : ""}>
+              <S.NavItem>SOBRE MIM</S.NavItem>
+              <S.NavItem>PORTFÓLIO</S.NavItem>
+              <S.NavItem>CONTATO</S.NavItem>
+            </S.NavItens>
+          </nav>
+
+          <S.HambIcon
+            className={isMenuOpen ? "menu_active" : ""}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          />
+        </S.ItensWrapper>
+
+        <S.MobileWrapper className={isMenuOpen ? "menu_active" : ""}>
+          <S.MobileItens>
+            <S.MobileItem>SOBRE MIM</S.MobileItem>
+            <S.MobileItem>PORTFÓLIO</S.MobileItem>
+            <S.MobileItem>CONTATO</S.MobileItem>
+          </S.MobileItens>
+        </S.MobileWrapper>
+      </Container>
+    </S.Header>
+  );
 };
 
 export default Header;
