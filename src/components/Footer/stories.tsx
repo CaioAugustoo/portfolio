053a6b3cc40149b91da85b946d7f59745366b0e5
Caio Augusto <caioamfr@gmail.com>
2020-12-18 00:34:49 -0300

    Delete stories.tsx

--- src/components/Footer/stories.tsx
+++
@@ -1,9 +0,0 @@
-import Footer from ".";
-import { Story, Meta } from "@storybook/react/types-6-0";
-
-export default {
-  title: "Footer",
-  component: Footer,
-} as Meta;
-
-export const Basic: Story = (args) => <Footer {...args} />;
