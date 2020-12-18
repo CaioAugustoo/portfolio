e68558ccc5a9daff53a985e8ba32557cbe2c70af
caioaugustoo <caioamfr@gmail.com>
2020-12-18 00:33:41 -0300

    Componente de footer

---
+++ src/components/Footer/styles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  height: auto;
+  width: 100%;
+  margin-top: 120px;
+`;
+
+export const Rights = styled.p`
+  font-size: 1.5rem;
+  font-weight: 500;
+  opacity: 0.7;
+  text-align: center;
+  margin: 0px 0 50px 0;
+`;
