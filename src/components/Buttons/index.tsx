18f68c77ca9de78dbd14b3ad35d3455e354e5300
Caio Augusto <caioamfr@gmail.com>
2020-12-18 00:34:22 -0300

    Delete index.tsx

--- src/components/Buttons/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from "./styles";
-import React from "react";
-import { ButtonProps } from "types/types";
-
-const Buttons = ({ children }: ButtonProps) => {
-  return <S.Button>{children}</S.Button>;
-};
-
-export default Buttons;
