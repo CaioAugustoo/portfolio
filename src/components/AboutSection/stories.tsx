11f063bdfdfc94f22acbfecfa5ead1a1e63374d5
Caio Augusto <caioamfr@gmail.com>
2020-12-18 00:34:09 -0300

    Delete stories.tsx

--- src/components/AboutSection/stories.tsx
+++
@@ -1,9 +0,0 @@
-import AboutSection from ".";
-import { Story, Meta } from "@storybook/react/types-6-0";
-
-export default {
-  title: "AboutSection",
-  component: AboutSection,
-} as Meta;
-
-export const Basic: Story = (args) => <AboutSection {...args} />;
