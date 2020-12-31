3e0196917f2cd1dd3be74302abe550c08bf62931
caioaugustoo <caioamfr@gmail.com>
2020-12-31 12:53:42 -0300

    Import do theme e do ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "styles/globals";
+import theme from "../styles/theme/theme";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <link rel="icon" href="/img/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -19,7 +21,10 @@ const App = ({ Component, pageProps }: AppProps) => {
           name="description"
           content="Desenvolvedor Front-end & UI/UX Designer."
         />
-        <meta name="google-site-verification" content="u6Vdueaj4zUT5QC9kQe57N0BopUpzrTN9HxiHTOZIBo" />
+        <meta
+          name="google-site-verification"
+          content="u6Vdueaj4zUT5QC9kQe57N0BopUpzrTN9HxiHTOZIBo"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://caiohtml.com/" />
         <meta
@@ -27,6 +32,7 @@ const App = ({ Component, pageProps }: AppProps) => {
           content="Caio Augusto | Desenvolvedor Front-end"
         />
         <meta name="theme-color" content="#4285f4" />
+        <meta name="background-color" content="#ffffff" />
         <meta
           property="og:description"
           content="Desenvolvedor Front-end & UI/UX Designer."
@@ -43,9 +49,9 @@ const App = ({ Component, pageProps }: AppProps) => {
         />
         <meta
           name="keywords"
-          content="front-end, desenvolvedor front-end, ui ux designer, designer gráfico, 
-            desenvolvedor front-end freelancer, desenvolvedor reactjs, desenvolvedor javascript, 
-            javascript, react, reactjs, typescript, strapi, api, dev, nextjs, cypress, tests, autoditada, 
+          content="front-end, desenvolvedor front-end, ui ux designer, designer gráfico,
+            desenvolvedor front-end freelancer, desenvolvedor reactjs, desenvolvedor javascript,
+            javascript, react, reactjs, typescript, strapi, api, dev, nextjs, cypress, tests, autoditada,
             caioaugusto, caioaugusto, caiohtml, caio augusto, freelancer, freela"
         />
         <meta name="robots" content="nofollow" />
@@ -56,7 +62,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       </Head>
       <Component {...pageProps} />
       <GlobalStyles />
-    </>
+    </ThemeProvider>
   );
 };
 export default App;
