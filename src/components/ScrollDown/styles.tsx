e34698a54955667410810d974f53214d11131cd1
caioaugustoo <caioamfr@gmail.com>
2020-12-31 12:57:09 -0300

    Uso das props fornecidas pelo ThemeProvider

--- src/components/ScrollDown/styles.tsx
+++ src/components/ScrollDown/styles.tsx
@@ -1,12 +1,14 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const ScrollDownArrow = styled.div`
   position: absolute;
   left: 50%;
   bottom: 0px;
+
   display: flex;
   margin-bottom: 3.1rem;
   cursor: pointer;
+
   img {
     width: 17px;
   }
