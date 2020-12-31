56d5c1845ca3406c47b329b1bb4418b47d2c30e7
caioaugustoo <caioamfr@gmail.com>
2020-12-31 12:52:10 -0300

    Uso das props fornecidas pelo ThemeProvider

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,5 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
-import { theme } from "./theme/theme";
+import styled, { createGlobalStyle, css } from "styled-components";
 
 // Resets
 const GlobalStyles = createGlobalStyle`
@@ -8,59 +7,67 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     outline: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
   :root {
     font-size: 62.5%;
   }
-  body, button {
-    transition: all .3s ease;
-    font-family: 'Montserrat', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-  body {
-    overflow-x: hidden !important;
-  }
-  ul li {
-    list-style: none;
-  }
-  a { 
-    text-decoration: none;
-    color: ${theme.colors.black}
-  }
-  
-  a, 
-  h1, 
-  p, 
-  h2,
-  h3 {
-    transition: all .3s ease;
-  }
-  header {
-    transition: all .3s ease;
-  }
-  body.dark-mode, 
-  body.dark-mode header, 
-  body.dark-mode #hmocIu  {
-    background-color: #1a1919;
-    color: #ffffff;
-    a, 
-    h1, 
-    p, 
-    li, 
+  ${({theme}) => css`
+    body.dark-mode,
+    body.dark-mode header,
+    body.dark-mode #hmocIu  {
+      background-color: ${theme.colors.black};
+      color: ${theme.colors.white};
+      a,
+      h1,
+      p,
+      li,
+      h2,
+      h3 {
+        color: ${theme.colors.white};
+      }
+    }
+    body, button {
+      transition: all .3s ease;
+      font-family: ${theme.font.family}
+    }
+    body {
+      overflow-x: hidden !important;
+    }
+    ul li {
+      list-style: none;
+    }
+    a {
+      text-decoration: none;
+      color: ${theme.colors.black}
+    }
+
+    a,
+    h1,
+    p,
     h2,
     h3 {
-      color: #ffffff;
+      transition: all .3s ease;
     }
-  }
+    header {
+      transition: all .3s ease;
+    }
+  `}
+
+
 `;
 export default GlobalStyles;
 
 // Styles
 export const Container = styled.div`
-  max-width: 120rem;
-  padding: 0 4rem;
-  margin: 0 auto;
-  width: 100%;
-  @media (max-width: ${theme.media.small}) {
-    padding: 0 2rem;
-  }
+  ${({theme}) => css`
+    max-width: 120rem;
+    padding: 0 4rem;
+    margin: 0 auto;
+    width: 100%;
+    @media (max-width: ${theme.media.small}) {
+      padding: 0 2rem;
+    }
+  `}
 `;
