eda985031c31d8252c6a80f28d9d592a2e074372
caioaugustoo <caioamfr@gmail.com>
2020-12-31 12:52:44 -0300

    Criado estilos e padrões adicionais

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,4 +1,4 @@
-export const theme = {
+export default {
   colors: {
     black: '#1a1919',
     primary: '#0185B2',
@@ -6,7 +6,30 @@ export const theme = {
     white: '#ffffff'
   },
   media: {
+    medium: '1400px',
     large: '1200px',
     small: '600px',
+  },
+  font: {
+    family: 'Montserrat, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif',
+    normal: 400,
+    normalx2: 500,
+    medium: 600,
+    bold: 700,
+    styles: {
+      italic: 'italic',
+    },
+    sizes: {
+      xxsmall: '1rem',
+      xxsmallx2: '1.15rem',
+      xxsmallx3: '1.2rem',
+      xsmall: '1.4rem',
+      xsmallx2: '1.5rem',
+      small: '1.7rem',
+      smallx2: '2.0rem',
+      medium: '2.5rem',
+      mediumx2: '3.7rem',
+      large: '8rem',
+    }
   }
 }
