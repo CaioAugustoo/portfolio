acb478cfcb831a50ba6812ad1f7ad2ebde695712
Caio Augusto <caioamfr@gmail.com>
2020-12-31 19:42:54 -0300

    'Uso das props fornecidas pelo ThemeProvider'

--- src/components/Titles/styles.tsx
+++ src/components/Titles/styles.tsx
@@ -1,34 +1,41 @@
-import styled from "styled-components";
-
-import { theme } from "../../styles/theme/theme";
+import styled, { css } from "styled-components";
 
 export const Title = styled.h2`
-  font-size: 2.5rem;
-  color: ${theme.colors.black};
-  text-align: center;
-  margin-bottom: 70px;
-  display: block;
-  &:before {
-    content: "";
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.medium};
+    text-align: center;
+    color: ${theme.colors.black};
+
+    margin-bottom: 70px;
     display: block;
-    margin: 0 auto;
-    width: 5px;
-    height: 27px;
-    position: relative;
-    top: 29px;
-    z-index: -1;
-    left: -90px;
-    transform: rotate(-5deg);
-    background: ${theme.colors.primary};
-    @media (max-width: ${theme.media.small}) {
-      width: 4px;
-      height: 23px;
-      top: 24px;
-      left: -75px;
+
+    &:before {
+      content: "";
+      display: block;
+      margin: 0 auto;
+
+      width: 5px;
+      height: 27px;
+
+      position: relative;
+      top: 29px;
+      left: -90px;
+
+      z-index: -1;
+      transform: rotate(-5deg);
+      background: ${theme.colors.primary};
+
+      @media (max-width: ${theme.media.small}) {
+        width: 4px;
+        height: 23px;
+        top: 24px;
+        left: -75px;
+      }
     }
-  }
-  @media (max-width: ${theme.media.small}) {
-    font-size: 2rem;
-    margin-bottom: 40px;
-  }
+
+    @media (max-width: ${theme.media.small}) {
+      font-size: ${theme.font.sizes.smallx2};
+      margin-bottom: 40px;
+    }
+  `}
 `;
