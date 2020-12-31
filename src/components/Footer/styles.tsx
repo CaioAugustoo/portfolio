2b7b92c4a86be61e4fe1edcecc853db7db8d8578
caioaugustoo <caioamfr@gmail.com>
2020-12-31 12:55:37 -0300

    Uso das props fornecidas pelo ThemeProvider

--- src/components/Footer/styles.tsx
+++ src/components/Footer/styles.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Footer = styled.footer`
   height: auto;
@@ -7,9 +7,11 @@ export const Footer = styled.footer`
 `;
 
 export const Rights = styled.p`
-  font-size: 1.5rem;
-  font-weight: 500;
-  opacity: 0.7;
-  text-align: center;
-  margin: 0px 0 50px 0;
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xsmallx2};
+    font-weight: ${theme.font.normalx2};
+    opacity: 0.7;
+    text-align: center;
+    margin: 0px 0 50px 0;
+  `}
 `;
