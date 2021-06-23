165804569d55f9158fee6aa41373c05d07d4f872
caioaugustoo <caioamfr@gmail.com>
2021-06-23 18:30:42 -0300

    Adicionada animações nos botões da página de contato

--- src/Templates/Contact/styles.ts
+++ src/Templates/Contact/styles.ts
@@ -1,5 +1,6 @@
 import * as ButtonStyles from "components/UI/Button/styles";
 import styled, { css } from "styled-components";
+import { fadeOpacity, fromBottom } from "styles/keyframes/keyframes";
 
 export const Wrapper = styled.main`
   ${({ theme }) => css`
@@ -11,6 +12,7 @@ export const Person = styled.div`
   ${({ theme }) => css`
     text-align: center;
     margin: ${theme.spacings.small} auto;
+    animation: ${fromBottom} 0.85s forwards;
 
     img {
       border-radius: 100%;
@@ -31,6 +33,7 @@ export const Links = styled.div`
 
     ${ButtonStyles.Button} {
       margin: ${theme.spacings.small} 0;
+      animation: ${fromBottom} 0.7s forwards;
     }
   `}
 `;
@@ -39,6 +42,7 @@ export const BackToHome = styled.div`
   ${({ theme }) => css`
     margin: ${theme.spacings.large} 0 0 0;
     text-align: center;
+    animation: ${fadeOpacity} 1s forwards;
 
     a {
       font-size: ${theme.font.sizes.xxxsmall2};
