fd6276ced6a417b127681bf3c9d1f962fed9dd0f
caioaugustoo <caioamfr@gmail.com>
2021-06-23 19:45:52 -0300

    Refatoração

--- src/components/Sections/Portfolio/styles.tsx
+++ src/components/Sections/Portfolio/styles.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { fromBottom } from "styles/keyframes/keyframes";
 
 export const Wrapper = styled.section`
